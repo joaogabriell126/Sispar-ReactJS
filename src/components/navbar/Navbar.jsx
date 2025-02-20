21aed685bc2aa58afeb6cb58db8ba8998cc8a698
Gabriel Oliveira <128335898+joaogabriell126@users.noreply.github.com>
2025-02-19 21:53:24 -0300

    Tentativa de correção da Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,13 +5,38 @@ import ReembolsosButton from "../../assets/Header/Botão - Reembolso.png"
 import PesquisaButton from "../../assets/Header/Botão - Pesquisa.png"
 import HistoricoButton from "../../assets/Header/Botão - Histórico.png"
 import ExitButton from "../../assets/Header/Botão - Sair.png"
+import Close from "../../assets/Header/imagem-fechar-header.png";
 
 function Navbar() {
     return (
         <>
-        <main className={styles.navbarContainer}>
-            <h2>oi</h2>
-        </main>
+            <nav className={styles.navbarContainer}>
+                <button>
+                    <img src={Close} alt="" />
+                </button>
+
+                <section>
+                    <img src={Perfil} alt="" />
+                    <div className={styles.boxButton}>
+                        <button>
+                            <img src={HomeButton} alt="" />
+                        </button>
+
+                        <button>
+                            <img src={ReembolsosButton} alt="" />
+                        </button>
+
+                        <button>
+                            <img src={PesquisaButton} alt="" />
+                        </button>
+
+                        <button>
+                            <img src={HistoricoButton} alt="" />
+                        </button>
+                    </div>
+                </section>
+                <img src={ExitButton} alt="" />
+            </nav>
         </>
     )
 }
--- src/components/solicitacao/Solicitacao.jsx
+++ src/components/solicitacao/Solicitacao.jsx
@@ -12,8 +12,50 @@ function Solicitacao() {
                 <img src={Seta} alt="" />
                 <p>Solicitação</p>
             </header>
+
             <main className={styles.mainSolicitacao}>
-                <h1>oi</h1>
+                <form className={styles.formSoliciatacao}>
+                    <div className={styles.grupo1}>
+                        <div className={styles.inputNome}>
+                            <label htmlFor="">Nome Completo</label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+
+                        <div className={styles.inputEmpresa}>
+                            <label htmlFor="">Empresa</label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+
+                        <div className={styles.inputPrestacao}>
+                            <label htmlFor="">N de Prest. Contas</label>
+                            <input type="number" name="name" id="name" />
+                        </div>
+
+                        <div className={styles.inputMotivo}>
+                            <label htmlFor="">Descrição / Motivo de Reembolso</label>
+                            <textarea name="" id=""></textarea>
+                        </div>
+                    </div>
+
+                    <div className={styles.verticalBar}></div>
+
+                    <div className={styles.grupo2}>
+                        <div className={styles.inputData}>
+                            <label htmlFor="">Data</label>
+                            <input type="date" name="" id="" />
+                        </div>
+
+                        <div className={styles.despesas}>
+                            <label htmlFor=""> Tipo de Despesa</label>
+
+                            <select name="" id="">
+                                <option value="">Selecionar </option>
+                                <option value=""> Alimentação</option>
+                                <option value="">Combustível</option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
             </main>
         </>
     )
